4877ea88d931b7e2f487e043d7b0e56ffc97ceae
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-02-02 10:45:09 +0100

    :hammer: /walcomming

---
+++ Commands/welcomming.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+const logs = require('../Functions/log.js')
+
+module.exports = {
+    name: "welcomming",
+    description: "ajout d'un salon de bienvenue",
+    permission: Discord.PermissionFlagsBits.ModerateMembers,
+    dm: false,
+    category: "Moderation",
+    options: [
+        {
+            type: "channel",
+            name: "salon",
+            description: "Salon bienvenue",
+            required: true,
+            autocomplete: true
+        }
+    ],
+
+    async run(client, message, args, db) {
+        logs.log(`[${new Date().toISOString()}]\t ${message.user.tag} a envoyé "${message}" dans le canal ${message.channel.name}`);
+
+
+        let channel = args.getChannel("salon")
+        if (!channel) return message.reply("Salon invalide")
+
+
+
+        try {
+            await db.run(`DELETE FROM Welcome WHERE serverID = ?`, [message.guild.id])
+
+            db.run(`INSERT INTO Welcome (channelID, serverID) VALUES (?, ?)`, [channel.id, message.guild.id])
+        } catch (err) {
+            return message.reply("Erreur sur la base de donnée, veuillez réessayer plus tard")
+        }
+
+        await message.reply(`Salon de bienvenue défini sur #${channel.name}`)
+    }
+}
