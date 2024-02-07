475ee903f3953db10144d53efbfc4f7e61f39220
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-02-07 10:46:15 +0100

    :bug: welcom patched

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -1,28 +1,10 @@
 const Discord = require('discord.js')
 
 module.exports = async (client, member) => {
-    let channel
-    let channelID
-
-    try {
-        await client.db.each('SELECT * from Welcome where serverID = ?', [member.guild.id], (err, rows) => {
-            if (!rows) {
-                console.log("No channel found")
-                return
-            } else {
-                console.log("rows : " + rows.channelID)
-                channelID = rows.channelID
-            }
-        });
-        channel = await client.channels.fetch(channelID)
-
-    } catch (err) {
-        console.log(err)
-    }
-
+    const channel = await fetchChannel(client, member);
 
     if (!channel) {
-        console.log(`no channel found`)
+        console.log(`not a channel`)
         return;
     }
 
@@ -30,11 +12,33 @@ module.exports = async (client, member) => {
 
     let Embed = new Discord.EmbedBuilder()
         .setTitle(`Bienvenue sur le serveur ${member.guild.name}`)
-        .setDescription(`Bienvenue sur le serveur ${member.guild.name}, ${member.user.tag} !`)
+        .setDescription(`Bienvenue sur le serveur ${member.guild.name} !`)
         .setColor(client.color)
         .setTimestamp()
         .setThumbnail(member.user.displayAvatarURL())
 
     channel.send({ embeds: [Embed] });
     //await message.reply({embeds : [Embed]});
+}
+
+// function to fetch channel
+function fetchChannel(client, member) {
+    return new Promise((resolve, reject) => {
+        try {
+            client.db.each('SELECT * from Welcome where serverID = ?', [member.guild.id], async (err, rows) => {
+                if (!rows) {
+                    console.log("No channel found");
+                    resolve(null);
+                } else {
+                    console.log("rows : " + rows.channelID);
+                    const channelID = rows.channelID;
+                    const channel = await client.channels.fetch(channelID);
+                    resolve(channel);
+                }
+            });
+        } catch (err) {
+            console.log(err);
+            reject(err);
+        }
+    });
 }
