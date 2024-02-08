def7a015d68dc4f774cf847da4cf381107ecb156
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-02-08 15:20:17 +0100

    :hammer: split role and welcome in 2

--- Commands/welcome_role.js
+++ Commands/welcome_role.js
@@ -2,34 +2,23 @@ const Discord = require('discord.js')
 const logs = require('../Functions/log.js')
 
 module.exports = {
-    name: "welcomming",
-    description: "ajout d'un salon de bienvenue",
+    name: "welcome_role",
+    description: "ajout d'un role automatique",
     permission: Discord.PermissionFlagsBits.ModerateMembers,
     dm: false,
     category: "Moderation",
     options: [
         {
-            type: "channel",
-            name: "salon",
-            description: "Salon bienvenue",
-            required: true,
-            autocomplete: true
-        },
-        {
             type: "role",
             name: "role",
-            description: "Role à donner",
-            required: false,
+            description: "Role a donner",
+            required: true,
             autocomplete: true
         }
     ],
 
     async run(client, message, args, db) {
         logs.log(`[${new Date().toISOString()}]\t ${message.user.tag} a envoyé "${message}" dans le canal ${message.channel.name}`);
-
-
-        let channel = args.getChannel("salon")
-        if (!channel) return message.reply("Salon invalide")
 
         let role = args.getRole("role")
         if (!role) {
@@ -45,13 +34,13 @@ module.exports = {
 
 
         try {
-            await db.run(`DELETE FROM Welcome WHERE serverID = ?`, [message.guild.id])
+            await db.run(`DELETE FROM Role WHERE serverID = ?`, [message.guild.id])
 
-            db.run(`INSERT INTO Welcome (channelID, roleID, serverID) VALUES (?, ?, ?)`, [channel.id, role.id, message.guild.id])
+            db.run(`INSERT INTO Role (roleID, serverID) VALUES (?, ?)`, [role.id, message.guild.id])
         } catch (err) {
             return message.reply("Erreur sur la base de donnée, veuillez réessayer plus tard")
         }
 
-        await message.reply(`Salon de bienvenue défini sur #${channel.name} ${role ? `avec le role ${role.name}` : ""}`)
+        await message.reply(`Role automatique sur @${role.name}`)
     }
 }
