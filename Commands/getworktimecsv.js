dbb2e5c5b0015dae6189b5bd2b7ed2fe75a8e6df
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-11-27 12:48:18 +0100

    :sparkles: add ID to recap

--- Commands/getworktimecsv.js
+++ Commands/getworktimecsv.js
@@ -14,10 +14,10 @@ module.exports = {
     async run(client, message, args, db) {
         logs.log(`[${new Date().toISOString()}]\t ${message.user.tag} a envoyé "${message}" dans le canal ${message.channel.name}`);
         //generate csv with database
-        let csv = "Date,Projet,Tache,Description,Time,User\n"
+        let csv = "ID,Date,Projet,Tache,Description,Time,User\n"
         const workTime = await db.WorkTime.findMany({ where: { serverID: message.guild.id } })
         workTime.forEach(wt => {
-            csv += `${wt.dateWorkTime},${wt.projet},"${wt.task}","${wt.comment}",${wt.workTime},"${client.users.cache.get(wt.userID)?.tag || 'Unknown User'}"\n`
+            csv += `${wt.workTimeID},${wt.dateWorkTime},${wt.projet},"${wt.task}","${wt.comment}",${wt.workTime},"${client.users.cache.get(wt.userID)?.tag || 'Unknown User'}"\n`
         })
         message.reply({ content: "Voici le fichier csv", files: [{ attachment: Buffer.from(csv), name: "workTime.csv" }] })
     }
