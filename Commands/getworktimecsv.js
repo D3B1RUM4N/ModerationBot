d991db429569e6e51ae9c9be739f2fe5478de05a
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-11-27 11:10:09 +0100

    :bug: fix csv

--- Commands/getworktimecsv.js
+++ Commands/getworktimecsv.js
@@ -17,7 +17,7 @@ module.exports = {
         let csv = "Date,Projet,Tache,Description,Time,User\n"
         const workTime = await db.WorkTime.findMany({ where: { serverID: message.guild.id } })
         workTime.forEach(wt => {
-            csv += `${wt.dateWorkTime},${wt.projet},${wt.task},${wt.comment},${wt.workTime},${client.users.cache.get(wt.userID)?.tag || 'Unknown User'}\n`
+            csv += `${wt.dateWorkTime},${wt.projet},"${wt.task}","${wt.comment}",${wt.workTime},"${client.users.cache.get(wt.userID)?.tag || 'Unknown User'}"\n`
         })
         message.reply({ content: "Voici le fichier csv", files: [{ attachment: Buffer.from(csv), name: "workTime.csv" }] })
     }
