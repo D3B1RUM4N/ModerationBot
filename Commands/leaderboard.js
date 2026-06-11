8c088fa0b07f6d541330d333a84d83ddfcbbe228
D3B1RUM4N <elies1.mek03@gmail.com>
2026-06-11 20:32:31 +0200

    :bug: catch errors

--- Commands/leaderboard.js
+++ Commands/leaderboard.js
@@ -17,19 +17,22 @@ module.exports = {
                 serverID: message.guild.id
             }
         })
-        if(!allXp) return message.reply("Aucun membre n'a d'xp")
+        if (!allXp) return message.reply("Aucun membre n'a d'xp")
 
         await message.deferReply()
 
-        let leaderboard = allXp.sort( async (a, b) => (await client.function.calculXP(b.xp, b.level)) - (await client.function.calculXP(a.xp, a.level)))
+        let leaderboard = allXp.sort(async (a, b) => (await client.function.calculXP(b.xp, b.level)) - (await client.function.calculXP(a.xp, a.level)))
 
         const Leaderboard = await new Canvas.Leaderboard()
-        .setBot(client)
-        .setGuild(message.guild)
-        .setBackground('public/image/leaderboard_background.png')
-        .setColorFont('#ffffff')
+            .setBot(client)
+            .setGuild(message.guild)
+            .setBackground('public/image/leaderboard_background.png')
+            .setColorFont('#ffffff')
+
+        if (leaderboard.length === 0) return message.followUp("Aucun membre n'a d'xp")
+
         for (let i = 0; i < 10 && i < leaderboard.length; i++) {
-            if(leaderboard[i]){
+            if (leaderboard[i]) {
                 let user = await client.users.fetch(leaderboard[i].userID)
                 Leaderboard.addUser(user, leaderboard[i].level, leaderboard[i].xp, (leaderboard[i].level + 1) * 100)
             }
@@ -37,6 +40,6 @@ module.exports = {
 
         const Image = await Leaderboard.toLeaderboard()
 
-        await message.followUp({files: [new Discord.AttachmentBuilder(Image.toBuffer(), 'leaderboard.png')]})
+        await message.followUp({ files: [new Discord.AttachmentBuilder(Image.toBuffer(), 'leaderboard.png')] })
     }
 }
